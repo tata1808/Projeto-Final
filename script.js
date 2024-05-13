612a6ad2afd93af6e95725b7906a5aa872dfd1e5
MaycoJunio <136364922+MaycoJunio@users.noreply.github.com>
2024-05-13 16:48:02 -0300

    git commit

--- script.js
+++ script.js
@@ -2,14 +2,12 @@
 const menuBtn = document.getElementById('menu-btn');
 const navbar = document.querySelector('.navbar');
 
-menuBtn.addEventListener('click', () => {
-    navbar.classList.toggle('show');
-});
+menuBtn.addEventListener
+('click', () => { navbar.classList.toggle('show')});
 
 // JavaScript para abrir e fechar a barra de pesquisa
 const searchBtn = document.getElementById('search-btn');
 const searchForm = document.querySelector('.search-form');
 
-searchBtn.addEventListener('click', () => {
-    searchForm.classList.toggle('show');
-});
+searchBtn.addEventListener
+('click', () => {searchForm.classList.toggle('show')});
