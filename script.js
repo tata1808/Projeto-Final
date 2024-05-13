d0175ecec7be64acc26ec160f0bd4fa8bd516be3
tata1808 <taina.f.dias@hotmail.com>
2024-05-12 23:19:35 -0300

    git commit

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
-let navbar = document.querySelector('.navbar');
+// JavaScript para abrir e fechar o menu mobile
+const menuBtn = document.getElementById('menu-btn');
+const navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
+menuBtn.addEventListener('click', () => {
+    navbar.classList.toggle('show');
+});
 
-let searchForm = document.querySelector('.search-form');
+// JavaScript para abrir e fechar a barra de pesquisa
+const searchBtn = document.getElementById('search-btn');
+const searchForm = document.querySelector('.search-form');
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    cartItem.classList.remove('active');
-}
-
-let cartItem = document.querySelector('.cart-items-container');
-
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-}
-
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
+searchBtn.addEventListener('click', () => {
+    searchForm.classList.toggle('show');
+});
