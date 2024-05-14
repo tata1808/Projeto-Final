7e3ee19969aff89362053786b49a200c3567b392
Tainá Dias <150836588+tata1808@users.noreply.github.com>
2024-05-14 13:03:02 -0300

    Add files via upload

---
+++ projeto/script.js
@@ -0,0 +1,19 @@
+{
+    "shippingLevelsOfService:"[
+      {
+        "serviceLevelId": 1,
+        "serviceLevelName": "Standard",
+        "unitShippingPrice": 2.99
+      },
+      {
+        "serviceLevelId": 3,
+        "serviceLevelName": "Expedited",
+        "unitShippingPrice": 10.99
+      },
+      {
+        "serviceLevelId": 5,
+        "serviceLevelName": "Express",
+        "unitShippingPrice": 14.99
+      }
+    ]
+}
